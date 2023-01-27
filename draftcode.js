bed9f0d562bd876971ad081d8cdbaa81c48d1a94
Gene Calm <ecalmrd@gmail.com>
2023-01-26 22:20:50 -0800

    final draft

---
+++ draftcode.js
@@ -0,0 +1,38 @@
+//create code to add/remove past, present, future classes
+// document.body.appendChild(tag);
+// document.createElement(tagName)
+// saveButton.addEventListener ("click", function() {
+//   localStorage.setItem(".saveBtn", );
+// var time = new Date();
+// var currentTime = time.getHours() + ":" + time.getMinutes() + ":" + time.getSeconds();
+// console.log(time);
+
+// var currentTime = new Date ($.now());
+// console.log(currentTime)
+
+
+// saveButton.addEventListener ("click", function() {
+//   localStorage.setItem(".saveBtn", );
+
+
+
+// $("#hour-10");
+// $("#hour-11").attr("data-time", moment("11:00 am", "hh:mm a").format("HH"));
+// $("#hour-12").attr("data-time", moment("12:00 pm", "hh:mm a").format("HH"));
+// $("#hour-13").attr("data-time", moment("1:00 pm", "h:mm a").format("HH"));
+// $("#hour-14").attr("data-time", moment("2:00 pm", "h:mm a").format("HH"));
+// $("#hour-15").attr("data-time", moment("3:00 pm", "h:mm a").format("HH"));
+// $("#hour-16").attr("data-time", moment("4:00 pm", "h:mm a").format("HH"));
+// $("#hour-17").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
+
+
+// if null then declare variable
+// if dis
+//   var getKey = localStorage.getItem("key");
+//   var getValue = localStorage.getItem("value");
+//       if (!getKey || !getValue) {
+//           return;
+//       }
+//   saveData.textContent = (getValue);
+
+
